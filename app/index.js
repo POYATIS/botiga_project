b974dca37a6db67ce745b20b0778cdbf9ae148d3
sandra.poyatos.7e4 <sandra.poyatos.7e4@itb.cat>
2023-05-03 19:09:59 +0200

    update_03-05

--- app/index.js
+++ app/index.js
@@ -4,15 +4,14 @@ const expressLoader = require('./express');
 const bodyParser = require('body-parser');
 const productRoutes = require('../routes');
 const path = require('path');
-
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../views');
 
 async function loadApp() {
 
-    app.use(express.static(path.join(__dirname, '..')));
-      
+    app.use(express.static(path.join(__dirname, '..', 'public')));
     expressLoader(app);
     app.use(bodyParser.json()); 
     app.use(bodyParser.urlencoded({ extended: true }));  
--- routes/shop/product.js
+++ routes/shop/product.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
 const { getAll, getById } = require('../../services/productService');
+const { create } = require('../../services/clienteService');
+
+router.post('/clientes', async (req, res, next) => {
+  try {
+    console.log("Se está ejecutando el POST");
+    const { nombre, apellidos, email, telefono, direccion } = req.body;
+    await create (nombre, apellidos, email, telefono, direccion);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use('/clientes', (req, res, next) => {
+  console.log("Solicitud recibida en /clientes");
+  next();
+});
+router.get('/clientes', (req, res) => {
+  res.render('clientes');
+});
 
 router.get('/', async (req, res, next) => {
   try {
